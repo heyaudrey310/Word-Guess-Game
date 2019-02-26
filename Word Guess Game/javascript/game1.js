9024c0c506fe3b22270d97248f09e140a1ce6ec6
heyaudrey310 <46985009+heyaudrey310@users.noreply.github.com>
2019-02-25 23:19:55 -0500

    Add files via upload

--- Word Guess Game/javascript/game1.js
+++ Word Guess Game/javascript/game1.js
@@ -27,19 +27,56 @@ $(function () {
     var winMessage = document.getElementById("winMessage");
     var loseMessage = document.getElementById("loseMessage");
 
-    
-    var word = words[Math.floor(Math.random() * words.length)];
+    // Let's get started//
+    var words = words[Math.floor(Math.random() * words.length)];
 
     var answerArray = [];
     for (var i = 0; i < word.length; i++) {
         answerArray[i] = " ";
     }
-    // letters left to be guessed//
-    var remainingLetters = word.length;
-    while (remainingLetters > 0) {
-        alert(answerArray.join(" "));
+   return answerArray;  {
+
+   }
+//    Guesses left "_" within HTML
+   var words = answerArray.join("_");
+   document.getElementById("guessesLeft").innerHTML =  words;
+   {
+    var guessLetter = document.getElementById("_").value;
+   }
+//    loop created to generate words from word array
+   if (word.length > 0) {
+       for(var i = 0; i < word.length; i++) {
+           if (words[i]=== letter)  {
+               answerArray[i] = letter;
+           }
+       }
+   }
+//    if letter entered is false
+   function guessLetter(letter) {
+    var guessLetter = false;
+    for (var i = 0; i < word.length; i++)    {
+        if (guessedLetter[i] == letter) {
+            guessedLetter = true;
+            // prompt should show up as "Almost"
+        }
     }
-    letters = document.getElementById("letters");
+   }
+   function guessLetter(letter) {
+    var guessLetter = false;
+    for (var i = 0; i < blankSpaces; i++)    {
+        if (guessedLetter[i] == letter) {
+            blankSpaces = true;
+    // prompt should show up as "Aw, sorry. Better luck next time!"
+
+// if letter entered is correct
+// } else if (guessesLeft === 0);...
+// prompt should show up as Excellent! You've won!
+// Images are supposed to show when user guessed the correct artist.
+
+
+   
+
+    
 
 
 
